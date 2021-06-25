1caf5a6528e96cddb9cb3b5abe29206193871297
Boyan Valchev <valchev.boian@gmail.com>
2021-06-25 20:38:51 +0300

    Preparing knn for demo

--- src/knn.py
+++ src/knn.py
@@ -8,8 +8,9 @@ import numpy as np
 import pandas as pd
 
 dataset = pd.read_csv('../data/fullData.csv')
-dataset = dataset.iloc[::-1].reset_index()
-dataset = dataset.sample(frac=1).reset_index(drop=True)
+#dataset = dataset.iloc[::-1].reset_index()
+#dataset = dataset.sample(frac=1).reset_index(drop=True)
+#dataset['message'].apply(lambda x: len(x.split(' '))>1)
 dataset['output'] = round(100 * dataset['movement_the_day_after'] / dataset['Close'])
 
 
@@ -32,8 +33,9 @@ def get_class(percent):
 
 
 class KNN_NLC_Classifer():
-    def __init__(self, k = 3):
+    def __init__(self, k, train_size):
         self.k = k
+        self.train_size = train_size
         self.vectorizer = TfidfVectorizer(stop_words='english')
 
 
@@ -75,13 +77,13 @@ class KNN_NLC_Classifer():
             step = 2000
             j = 0
 
-            if x_test[i + train_size + 1] != '' and x_test[i + train_size + 1] is not None:
+            if x_test[i + self.train_size + 1] != '' and x_test[i + self.train_size + 1] is not None:
 
                 while(j < self.x_train.shape[0]):
                     if (j +1 >= self.x_train.shape[0]):
                         j += 1
                     elif (j + step > self.x_train.shape[0]):
-                        document = self.x_test[i + train_size + 1]
+                        document = self.x_test[i + self.train_size + 1]
                         sim_obj = self.get_most_similar_document(document, j, self.x_train.shape[0] - 1)
                         if sim_obj['temp_sim'] > max_sim:
                             max_similarities.append(sim_obj['temp_sim'])
@@ -89,7 +91,7 @@ class KNN_NLC_Classifer():
                             max_indexes = sorted(zip(max_similarities, max_indexes), reverse=True)[:self.k]
                         j = self.x_train.shape[0]
                     else:
-                        document = self.x_test[i + train_size + 1]
+                        document = self.x_test[i + self.train_size + 1]
                         sim_obj = self.get_most_similar_document(document, j, j + step - 1)
                         if sim_obj['temp_sim'] > max_sim:
                             max_similarities.append(sim_obj['temp_sim'])
@@ -106,45 +108,74 @@ class KNN_NLC_Classifer():
 
             #prediction = max(set(pred_result), key=pred_result.count)
 
-            prediction = round(temp_sum/len(pred_result))
+            value = round(temp_sum/len(pred_result))
 
-            prediction = get_class(prediction)
+
+            prediction = get_class(value)
             y_predict.append(prediction)
-
             print(prediction)
-            print(get_class(test_corpus.loc[train_size + i + 1, 'output']))
-            print(test_corpus.loc[train_size + i + 1, 'output'])
+            print(value)
+            #print(get_class(self.x_test.loc[self.train_size + i + 1, 'output']))
 
-            print(prediction == get_class(test_corpus.loc[train_size + i + 1, 'output']))
+            #print(prediction == get_class(self.x_test.loc[self.train_size + i + 1, 'output']))
 
             toc = time.perf_counter()
             print(f"The operation took {toc - tic:0.4f} seconds")
         return y_predict
 
-train_size = int(0.998 * len(dataset))
-test_size = int(0.002 * len(dataset))
-print(f"Train size: {train_size}, Test Size: {test_size}")
 
-train_corpus = dataset[:train_size]
-test_corpus = dataset[-test_size:]
+def check_knn_accuracy():
+    train_size = int(0.998 * len(dataset))
+    test_size = int(0.002 * len(dataset))
+    print(f"Train size: {train_size}, Test Size: {test_size}")
 
-X_train = train_corpus['message']
-y_train = train_corpus['output']
+    train_corpus = dataset[:train_size]
+    test_corpus = dataset[-test_size:]
+
+    X_train = train_corpus['message']
+    y_train = train_corpus['output']
 
 
-classifier = KNN_NLC_Classifer(k=3)
+    classifier = KNN_NLC_Classifer(3, train_size)
 
-classifier.fit(X_train, y_train)
+    classifier.fit(X_train, y_train)
 
-print(test_corpus.head())
+    print(test_corpus.head())
 
-y_pred_final = classifier.predict(test_corpus['message'])
+    y_pred_final = classifier.predict(test_corpus['message'])
 
-num_all = len(test_corpus)
-num_correct = 0
+    num_all = len(test_corpus)
+    num_correct = 0
 
-for j in range(len(y_pred_final)):
-    if y_pred_final[j] == get_class(test_corpus.loc[train_size + j + 1, 'output']):
-        num_correct += 1
+    for j in range(len(y_pred_final)):
+        if y_pred_final[j] == get_class(test_corpus.loc[train_size + j + 1, 'output']):
+            num_correct += 1
 
-print(num_correct/num_all)
+    print(num_correct/num_all)
+
+def demo_knn():
+    train_corpus = dataset
+    X_train = train_corpus['message']
+    y_train = train_corpus['output']
+
+    test_corpus = [
+        'Bitcoin to the moon',
+        'I love Bitcoin',
+        'Bitcoin will go up',
+        'Bitcoin will collapse'
+    ]
+
+    classifier = KNN_NLC_Classifer(1, -1)
+    classifier2 = KNN_NLC_Classifer(5, -1)
+
+    classifier.fit(X_train, y_train)
+    classifier.predict(test_corpus)
+
+    classifier2.fit(X_train, y_train)
+    classifier2.predict(test_corpus)
+
+
+
+#check_knn_accuracy()
+
+demo_knn()
